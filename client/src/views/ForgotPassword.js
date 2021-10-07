af14f704b988689f4ec82d9ac5ffbc05078acc7c
skg1998 <sahilkg1998@gmail.com>
2021-10-07 23:13:30 +0530

    feat(): work on forget password page

--- client/src/views/ForgotPassword.js
+++ client/src/views/ForgotPassword.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const ForgotPassword = () => {
-    return <h1>ForgotPassword</h1>
-}
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: theme.spacing(2),
+
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '300px',
+        },
+        '& .MuiButtonBase-root': {
+            margin: theme.spacing(2),
+        },
+    },
+}));
+
+const ForgotPassword = (props) => {
+    const classes = useStyles();
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(email);
+    };
+
+    return (
+        <form className={classes.root} onSubmit={handleSubmit}>
+            <TextField
+                label="Email"
+                variant="filled"
+                type="email"
+                required
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+            <div>
+                <Button type="submit" variant="contained" color="primary">
+                    Send
+                </Button>
+            </div>
+        </form>
+    );
+};
 
 export default ForgotPassword;
