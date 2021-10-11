b83e6542dd57e98839c333742b645191c6968dcd
skg1998 <sahilkg1998@gmail.com>
2021-10-11 22:45:05 +0530

    feat(category-model): setup category model

---
+++ server/src/models/category.model.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('category', {
+        id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4
+        },
+        name: { type: DataTypes.STRING },
+        desc: { type: DataTypes.STRING }
+    }, {});
+    Category.associate = function (models) {
+        // associations can be defined here
+    };
+    return Category;
+};
