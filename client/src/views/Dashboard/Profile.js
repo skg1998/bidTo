e70c1f4bd86c8667ef54d1338fef25a85c98804d
NiteshKumar <nk6511165@gmail.com>
2021-10-21 13:31:48 +0530

    'profile-page-updated'

--- client/src/views/Dashboard/Profile.js
+++ client/src/views/Dashboard/Profile.js
@@ -1,7 +1,93 @@
 import React from 'react';
+import {
+    Box,
+    Container,
+    Grid,
+    Paper
+} from '@material-ui/core';
+
+import AccountProfile from '../Profile/AccountProfile';
+import AccountProfileDetails from '../Profile/AccountProfileDetail';
+import ColumnGroupingTable from './Table';
+import SelfTable from './SelfTable';
+import ProductChart from './ProductChart';
+import SelfChart from './SelfChart'
+
 
 const Profile = () => {
-    return <h1>Profile</h1>
+    return (
+        <>
+            
+            <Box
+                sx={{
+                    backgroundColor: 'background.default',
+                    minHeight: '100%',
+                    py: 3
+                }}
+                style={{ margin: '15px' }}
+            >
+                <Container maxWidth="lg" xs={12} lg={12} md={12}>
+                    <Grid
+                        container
+                        spacing={3}
+                    >
+                        <Grid
+                            item
+                            lg={4}
+                            md={6}
+                            xs={12}
+                        >
+                            <AccountProfile />
+                        </Grid>
+                        <Grid
+                            item
+                            lg={8}
+                            md={6}
+                            xs={12}
+                        >
+                            <AccountProfileDetails/>
+                        </Grid>
+                    </Grid>
+                </Container>
+
+                <Container style={{marginTop:"3%"}}>
+                    <Grid container spacing={3} style={{height:"20%"}}>
+                        
+                        <Grid item lg={6} md={6} xs={12} style={{height:"100%"}}>
+                           <Paper>
+                              <ProductChart/>
+                           </Paper>
+                        </Grid>
+                       
+                        <Grid item lg={6} md={6} xs={12}  >
+                            <Paper>
+                              <ProductChart/>
+                           </Paper>
+                        </Grid>
+                    </Grid>
+
+                   
+                </Container>
+
+                <Container style={{marginTop:"3%"}}>
+                    <Grid container spacing={3}>
+                        <Grid item lg={6} md={6} xs={12}>
+                           <ColumnGroupingTable/>
+                        </Grid>
+                        <Grid item lg={6} md={6} xs={12}>
+                           <SelfTable/>
+                        </Grid>
+                    </Grid>
+
+                   
+                </Container>
+               
+            </Box>
+            
+                
+            
+        </>
+    )
 }
 
 export default Profile;
