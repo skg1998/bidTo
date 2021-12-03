087998eebaffa54a1ccbc13ff0fc5a6ecc8f562e
skg1998 <sahilkg1998@gmail.com>
2021-12-03 14:35:53 +0530

    feat(wishlist): add wishlist components to view

---
+++ client/src/views/WishList/wishList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import WishListContainer from '../../container/WishList/wishListContainer';
+
+const WishList = (props) => {
+    return (
+        <WishListContainer />
+    )
+}
+
+export default WishList;
