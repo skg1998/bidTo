d7601612b75512b6eda5ccd4984317b3c0284fe3
skg1998 <sahilkg1998@gmail.com>
2021-12-03 14:24:24 +0530

    feat(checkout): create checkout functionality

---
+++ client/src/views/Checkout/BidCheckoutDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CartContainer from '../../container/Cart/CartContainer';
+
+const Checkout = () => {
+  return (
+    <CartContainer />
+  )
+}
+
+export default Checkout;
