ddf4d1f4684700b20937cb4ab04f0caa90bfda02
skg1998 <sahilkg1998@gmail.com>
2021-12-03 14:15:02 +0530

    feat(wishlist): add wishlist functionality

---
+++ client/src/components/WishList/WishList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Favorite = (props) => {
+    return (
+        <div></div>
+    )
+}
+
+export default Favorite;
---
+++ client/src/components/WishList/WishListItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FavoriteItem = (props) => {
+    return (
+        <div></div>
+    )
+}
+
+export default FavoriteItem;
